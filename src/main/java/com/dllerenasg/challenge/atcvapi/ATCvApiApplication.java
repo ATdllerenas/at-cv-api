78960b4f17b0f9bc8a03623f392d4883fb488bdb
Dayana Llerenas <dayana.llerenas@agilethought.com>
2021-05-26 16:49:57 -0500

    Create Spring Boot API CV

---
+++ src/main/java/com/dllerenasg/challenge/atcvapi/ATCvApiApplication.java
@@ -0,0 +1,13 @@
+package com.dllerenasg.challenge.atcvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATCvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ATCvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dllerenasg/challenge/atcvapi/ATCvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dllerenasg.challenge.atcvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATCvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
