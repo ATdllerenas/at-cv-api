a7fe0a9575d1b9087e5bed9567415f8d0f9b678b
Dayana Llerenas <dayana.llerenas@agilethought.com>
2021-05-27 18:06:31 -0500

    Added Default serialVersionUID

--- src/main/java/com/dllerenasg/challenge/atcvapi/exception/CVMissingRequiredFieldException.java
+++ src/main/java/com/dllerenasg/challenge/atcvapi/exception/CVMissingRequiredFieldException.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CVMissingRequiredFieldException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
 	public CVMissingRequiredFieldException(String messageDetails) {
 		super(messageDetails);
 		}
--- src/main/java/com/dllerenasg/challenge/atcvapi/exception/CVNotFoundException.java
+++ src/main/java/com/dllerenasg/challenge/atcvapi/exception/CVNotFoundException.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CVNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
 	public CVNotFoundException(String messageDetails) {
 		super(messageDetails);
 		}
